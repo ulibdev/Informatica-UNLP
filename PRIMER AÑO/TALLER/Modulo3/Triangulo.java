cdc85dfb0bcb8dcc5e8a311ac3ca375f37586161
ulib <ulisesbdeveloper@gmail.com>
2025-10-01 20:26:30 -0300

    Update Triangulo.java

--- PRIMER AÑO/TALLER/Modulo3/Triangulo.java
+++ PRIMER AÑO/TALLER/Modulo3/Triangulo.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tema3;
 
-/**
- *
- * @author ulise
- */
 public class Triangulo {
     private double lado1;
     private double lado2;
