c9a67c6757bd71655fe70e97b945f287043b05ff
ulib <ulisesbdeveloper@gmail.com>
2026-03-17 22:13:23 -0300

    Create Ej9.cs

---
+++ SEGUNDO AÑO/.NET/2/Practica2/Ej9.cs
@@ -0,0 +1,51 @@
+﻿/*
+8) Investigar acerca de la clase StringBuilder del espacio de nombre System.Text ¿En qué
+circunstancias es preferible utilizar StringBuilder en lugar de utilizar string? Implementar un
+caso de ejemplo en el que el rendimiento sea claramente superior utilizando StringBuilder en lugar
+de string y otro en el que no.
+
+el stringbuilder es mejor a la hora de modificar string porque los string son inmutables,
+es decir que cuando uno lo quiere modificar en realidad lo que se hace es hacer un string
+totalmente nuevo ocupando otro espacio en memoria y perder lo anterior
+
+StringBuilder.Append	
+agrega informacion al final
+
+StringBuilder.Insert	
+inserta
+
+StringBuilder.Remove	
+remueve
+
+StringBuilder.Replace	
+reemplaza
+
+
+9) Investigar sobre el tipo DateTime y usarlo para medir el tiempo de ejecución de los algoritmos
+implementados en el ejercicio anterior.
+*/
+
+using System.Text;
+
+
+//en este caso gana StringBuilder
+DateTime inicio = DateTime.Now;
+string hola = ".";
+for(int i = 0; i< 100; i++)
+{
+    hola = hola + "hola";
+}
+DateTime fin = DateTime.Now;
+Console.WriteLine((fin-inicio).Milliseconds);
+inicio = DateTime.Now;
+StringBuilder constructor = new StringBuilder(".");
+for(int i = 0; i< 100; i++)
+{
+    constructor.Append("hola");
+}
+fin = DateTime.Now;
+Console.WriteLine((fin-inicio).Milliseconds);
+Console.WriteLine(hola);
+Console.WriteLine(constructor);
+
+//pero si se quiere hacer algo simple solo una vez gana string
