82f5d3faf8e2807077c1eabb5e18f5d2e5715690
ulib <ulisesbdeveloper@gmail.com>
2026-03-18 14:19:38 -0300

    15y16

---
+++ SEGUNDO AÑO/.NET/2/Practica2/Ej15.cs
@@ -0,0 +1,17 @@
+﻿/*
+15) Determinar qué hace el siguiente programa y explicar qué sucede si no se pasan parámetros cuando
+se invoca desde la línea de comandos.
+Console.WriteLine("¡Hola {0}!", args[0]);
+
+Basicamente {0} es un placeholder asi que al pasarle args[0] toma el primer parametro que le pasamos,
+pero si le pasamos otro paramentro este ira la siguiente posicion asi que habria que pasar args[1],
+ademas van por orden
+
+si no pasamos parametro tira error System.IndexOutOfRangeException que dice que nos fuimos del limite
+del array
+
+*/
+
+Console.WriteLine("¡Hola {0}!", args[0]);
+Console.WriteLine("Adios {0}", args[1]);
+
---
+++ SEGUNDO AÑO/.NET/2/Practica2/Ej16.cs
@@ -0,0 +1,20 @@
+﻿/*
+16) Escribir un programa que reciba una lista de nombres como parámetro por la línea de comandos e
+imprima por consola un saludo personalizado para cada uno de ellos.
+a) Utilizando la sentencia for
+b) Utilizando la sentencia foreach
+
+*/
+
+//a) con args.Length es la cantidad de elementos asi que utilizo eso para calcular el limite
+
+for(int i=0;i<= args.Length-1; i++)
+{
+    Console.WriteLine("Holaaaa {0}",args[i]);
+}
+//b)
+foreach(string a in args)
+{
+    Console.WriteLine("Holaaaa " + a);
+}
+
