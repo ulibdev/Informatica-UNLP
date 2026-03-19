0ca728e8d5854c877bc2299c4fd12b3558c857e1
ulib <ulisesbdeveloper@gmail.com>
2026-03-19 19:48:12 -0300

    Create Ej17.cs

---
+++ SEGUNDO AÑO/.NET/2/Practica2/Ej17.cs
@@ -0,0 +1,16 @@
+﻿
+int n = int.Parse(Console.ReadLine());
+for(int i= 1; i< n; i++)
+{
+    if(EsPrimo(i))Console.WriteLine(i);
+}
+
+bool EsPrimo(int n)
+{
+    if(n <= 1)return false;
+    else for (int i = 2;i<= Math.Sqrt(n); i++)
+    {
+        if(n%i == 0)return false;
+    }
+    return true;
+}
