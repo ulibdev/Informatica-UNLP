0e6865f91f2cdae7e6b2439e36acf3368952de4b
ulib <ulisesbdeveloper@gmail.com>
2026-03-20 12:40:13 -0300

    acomodar numeros y ej 21

---
+++ SEGUNDO AÑO/.NET/2/Practica2/Ej21.cs
@@ -0,0 +1,31 @@
+﻿
+/*
+21) Codificar el método Imprimir para que el siguiente código produzca la salida por consola que se
+observa. Considerar que el usuario del método Imprimir podría querer más adelante imprimir otros
+datos, posiblemente de otros tipos pasando una cantidad distinta de parámetros cada vez que invoque el
+método. Tip: usar params
+
+Imprimir(1, "casa", 'A', 3.4, DayOfWeek.Saturday);
+Imprimir(1, 2, "tres");
+Imprimir();
+Imprimir("-------------");
+
+1 casa A 3,4 Saturday
+1 2 tres
+*/
+
+
+void Imprimir (params object[] vector)
+{
+    foreach (object s in vector)
+    {
+        Console.Write(s);
+        Console.Write(" ");
+    }
+    Console.WriteLine();
+}
+
+Imprimir(1, "casa", 'A', 3.4, DayOfWeek.Saturday);
+Imprimir(1, 2, "tres");
+Imprimir();
+Imprimir("-------------");
