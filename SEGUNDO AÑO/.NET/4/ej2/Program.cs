e67622f7498d723ca8582dc024ed75d790fc9d59
ulib <ulisesbdeveloper@gmail.com>
2026-04-08 18:34:00 -0300

    Update ej2

--- SEGUNDO AÑO/.NET/4/ej2/Program.cs
+++ SEGUNDO AÑO/.NET/4/ej2/Program.cs
@@ -1,15 +1,7 @@
 ﻿/*
-1) Definir una clase Persona con 3 campos públicos: Nombre, Edad y DNI. Escribir un algoritmo que
-permita al usuario ingresar en una consola una serie de datos de esta forma:
-Nombre,Documento,Edad <ENTER>.
-
-Una vez finalizada la entrada de datos, el programa debe imprimir en la consola un listado con 4
-columnas de la siguiente forma:
-Nro) Nombre Edad DNI.
-Ejemplo de listado por consola:
-1) Juan Perez 40 2098745
-2) José García 41 1965412
-...
+2) Modificar el programa anterior haciendo privados todos los campos. Definir un constructor adecuado
+y un método público Imprimir() que escriba en la consola los campos del objeto con el formato
+requerido para el listado.
 */
 
 using teoria2;
